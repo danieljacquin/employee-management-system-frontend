9ecc09bc99a545bd450323ce67ad753c4d88153d
djacquin <danielcamilo853@gmail.com>
2026-01-14 17:55:19 -0500

    feat(ui): add global toast notifications provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from 'react-router-dom';
 
+import ToastNotification from './shared/components/ui/ToastNotification';
 import TankStackQueryProvider from './shared/providers/TankStackQueryProvider';
 import { router } from './shared/routes/router';
 
 function App() {
   return (
     <TankStackQueryProvider>
+      <ToastNotification />
       <RouterProvider router={router} />
     </TankStackQueryProvider>
   );
