64026f4224acf4ee4b5b35754bd8a328766394f7
djacquin <danielcamilo853@gmail.com>
2026-01-16 01:06:34 -0500

    feat(layout): add sidebar and grid layout to MainLayout

--- src/shared/components/layout/MainLayout.tsx
+++ src/shared/components/layout/MainLayout.tsx
@@ -1,15 +1,22 @@
+import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from '../navigation/Navbar';
+import Sidebar from '../navigation/SideBar';
 
 const MainLayout = () => {
   return (
-    <>
+    <div className="grid grid-rows-[64px_1fr] h-screen">
       <Navbar />
-      <main className="max-w-7xl mx-auto py-8 px-4">
-        <Outlet />
-      </main>
-    </>
+      <div className="grid grid-cols-[230px_1fr]">
+        <Sidebar />
+        <main className="flex flex-col py-8 px-6 overflow-hidden min-h-0">
+          <Suspense fallback={null}>
+            <Outlet />
+          </Suspense>
+        </main>
+      </div>
+    </div>
   );
 };
 
